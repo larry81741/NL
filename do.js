27bfc5e04b6c739f4a32ab02b28d7194fcd55d58
DTDO <91510137+jeff81030@users.noreply.github.com>
2021-11-23 11:54:27 +0800

    do

--- do.js
+++ do.js
@@ -9,3 +9,20 @@ function showPage() {
   document.getElementById("all").style.display = "block";
 }
 
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+
+var swiper = new Swiper(".domySwiper", {
+
+  navigation: {
+    nextEl: "#dobuttonright",
+    prevEl: "#dobuttonleft",
+  },
+  loop: true,
+  slidesPerView: 3,
+  speed: 300,
+
+
+
+});
